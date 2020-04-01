ea1a49740d17100dff1dcd1763ea395cb7e58775
Catherine231 <59292876+Catherine231@users.noreply.github.com>
2020-04-02 00:35:23 +0200

    Running code
    we can add the type of the user through the url

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -14,6 +14,7 @@ public class User {
     private String username;
     private String password;
     private String email;
+    public String type;
 
     public User(String username, String password, String email) {
     	super();
@@ -47,5 +48,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
