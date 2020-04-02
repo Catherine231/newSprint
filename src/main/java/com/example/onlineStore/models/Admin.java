9d0a051df3585d84b54a4c180ed280f265682473
ayaamr11 <36492846+ayaamr11@users.noreply.github.com>
2020-04-02 21:35:01 +0200

    admin updates

--- src/main/java/com/example/onlineStore/models/Admin.java
+++ src/main/java/com/example/onlineStore/models/Admin.java
@@ -2,5 +2,17 @@ package com.example.onlineStore.models;
 
 public class Admin extends User
 {
-
+	void manage_brand() {
+		
+	}
+	void store_data() {
+		
+	}
+	void add_product() {
+		
+	}
+	void provide_voucher() {
+		
+	}
+	void  
 }
